5f80b9e7a968baa5576c24cdea9425a91a7606e6
koomook <bongbonggg97@gmail.com>
2018-10-16 21:07:13 +0900

    Update github client id

--- frontend/src/components/GithubAuth/GithubLoginButton.js
+++ frontend/src/components/GithubAuth/GithubLoginButton.js
@@ -7,16 +7,7 @@ const GithubLoginButton = props => {
     props.sendGithubCode(response);
   };
   const onFailure = response => console.error(response);
-  return (
-    <GitHubLogin
-      clientId="f5559a879f2aff086280"
-      onSuccess={onSuccess}
-      onFailure={onFailure}
-      redirectUri=""
-      buttonText="Login with Github"
-      className="fa fa-github btn btn-primary"
-    />
-  );
+  return <GitHubLogin clientId="aa6d97dfaa1401631ba4" onSuccess={onSuccess} onFailure={onFailure} redirectUri="" buttonText="Login with Github" className="fa fa-github btn btn-primary" />;
 };
 
 export default GithubLoginButton;
